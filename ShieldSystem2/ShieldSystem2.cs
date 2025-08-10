27db25e2958045a2b32cbd023f23bf929b3cf39a
goldwizards <118609106+goldwizards@users.noreply.github.com>
2025-08-10 12:29:25 +0900

    Update ShieldSystem2.cs

--- ShieldSystem2/ShieldSystem2.cs
+++ ShieldSystem2/ShieldSystem2.cs
@@ -2,5 +2,18 @@ using Terraria.ModLoader;
 
 namespace ShieldSystem2
 {
-    public class ShieldSystem2 : Mod { }
+    public class ShieldSystem2 : Mod
+    {
+        public static bool CalamityLoaded { get; private set; }
+
+        public override void Load()
+        {
+            CalamityLoaded = ModLoader.TryGetMod("CalamityMod", out _);
+        }
+
+        public override void Unload()
+        {
+            CalamityLoaded = false;
+        }
+    }
 }
